61524e6d17fa28c0ec759d944fce864d60a78141
Eduardo Matheus Oliveira de Córdova <eduardocordova21@outlook.com.br>
2023-05-11 17:36:18 -0300

    Create exceptions.dart

---
+++ lib/core/errors/exceptions.dart
@@ -0,0 +1,13 @@
+class ApiException implements Exception {
+  final String message;
+  final int? statusCode;
+
+  ApiException({required this.message, this.statusCode});
+}
+
+class GeneralException implements Exception {
+  final String message;
+
+  GeneralException(
+      {this.message = "Ocorreu um erro, por favor tente novamente!"});
+}
