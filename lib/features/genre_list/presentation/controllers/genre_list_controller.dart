084f6e3c799838ce60740f0bdf8dd4d93992e1ea
Eduardo Matheus Oliveira de Córdova <eduardocordova21@outlook.com.br>
2023-05-15 00:40:08 -0300

    Adicionando componentes necessários para o Genre List

--- lib/features/genre_list/presentation/controllers/genre_list_controller.dart
+++ lib/features/genre_list/presentation/controllers/genre_list_controller.dart
@@ -1,1 +1,39 @@
-class GenreListController {}
+import 'package:get/get.dart';
+import 'package:music_app/shared/models/genre_model.dart';
+
+import '../../../../core/errors/failures.dart';
+import '../../../../core/mixins/screen_loading_and_error_mixin.dart';
+import '../../data/repositories/genre_list_repository.dart';
+
+class GenreListController extends GetxController
+    with ScreenLoadingAndErrorMixin {
+  final GenreListRepository _genreListRepository;
+  final RxList<GenreModel> genres = RxList([]);
+
+  GenreListController(GenreListRepository genreListRepository)
+      : _genreListRepository = genreListRepository;
+
+  @override
+  void onInit() {
+    getGenreList();
+    super.onInit();
+  }
+
+  Future<void> getGenreList() async {
+    setLoadingToTrue();
+    setError(null);
+
+    final getGenresResponse = await _genreListRepository.getGenreList();
+
+    getGenresResponse.fold((Failure failureResponse) {
+      if (failureResponse is GetGenreListFailure) {
+        setError(failureResponse.message);
+      }
+    }, (List<GenreModel> genreResponse) {
+      print(genreResponse);
+      genres.value = genreResponse;
+    });
+
+    setLoadingToFalse();
+  }
+}
--- lib/features/genre_list/presentation/screens/genre_list_screen.dart
+++ lib/features/genre_list/presentation/screens/genre_list_screen.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_instance/get_instance.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+import 'package:get/route_manager.dart';
+import 'package:music_app/features/genre_details_list/presentation/screens/genre_details_screen.dart';
+import 'package:music_app/features/genre_list/presentation/controllers/genre_list_controller.dart';
 import 'package:music_app/shared/widgets/screen_widget.dart';
+
+import '../../../../shared/widgets/img_and_title_row_widget.dart';
 
 class GenreListScreen extends StatelessWidget {
   static const routeName = "/genre-list";
@@ -8,12 +15,41 @@ class GenreListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenWidget(
-      isLoading: false,
-      title: "Lista de Genêros",
-      error: "Erro ao carregar lista de genêros",
-      onTryAgain: () {},
-      child: Container(),
+    final genreListCtlr = Get.find<GenreListController>();
+    return Obx(
+      () => ScreenWidget(
+        isLoading: false,
+        title: "Lista de Genêros",
+        child: Column(children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: ListView.separated(
+                itemBuilder: (_, index) {
+                  final genre = genreListCtlr.genres[index];
+                  return InkWell(
+                    onTap: () => Get.toNamed(
+                        "${GenreListScreen.routeName}${GenreDetailsScreen.routeName}",
+                        arguments: genre),
+                    child: ImgAndTitleRowWidget(
+                      title: genre.title,
+                      heroTag: genre.title,
+                      img: genre.img,
+                    ),
+                  );
+                },
+                separatorBuilder: (_, __) {
+                  return const SizedBox(height: 12);
+                },
+                itemCount: genreListCtlr.genres.length,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+        ]),
+      ),
     );
   }
 }
